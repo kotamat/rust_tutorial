7b7143722d49c6a2e72f145a39f20aa39075f132
kotamat <kota1861@gmail.com>
2021-06-27 22:04:27 +0900

    add modules

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,45 @@
+use crate::back_of_house::Appetizer::{Salad, Soup};
+use crate::back_of_house::Breakfast;
+
+mod front_of_house;
+pub use crate::front_of_house::hosting::add_to_waitlist;
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: toast.to_string(),
+                seasonal_fruit: "peaches".to_string()
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad
+    }
+}
+
+pub fn eat_at_restaurant () {
+    // let mut meal = back_of_house::Breakfast::summer("Rye");
+    let mut meal = Breakfast::summer("Rye");
+    meal.toast = "Wheat".to_string();
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = Soup;
+    let order2 = Salad;
+}
---
+++ restaurant/src/main.rs
@@ -0,0 +1,6 @@
+use restaurant::{eat_at_restaurant, add_to_waitlist};
+
+fn main() {
+    eat_at_restaurant();
+    add_to_waitlist();
+}
