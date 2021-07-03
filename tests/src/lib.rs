8a5c8f926bfe23dac5e9dede8b5ce1d045205e11
kotamat <kota1861@gmail.com>
2021-07-03 14:16:10 +0900

    testing

---
+++ tests/src/lib.rs
@@ -0,0 +1,84 @@
+mod test_organization;
+
+#[cfg(test)]
+mod tests {
+    use crate::Rectangle;
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100, got 200")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn greeting(name: &str) -> String {
+    // format!("Hello {}!", name)
+    format!("Hello!")
+}
+
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value)
+        }
+        Guess { value }
+    }
+}
+
+pub fn add_two(x: i32) -> i32 {
+    x + 2
+}
